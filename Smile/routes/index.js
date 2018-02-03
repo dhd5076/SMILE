56a4de9a2bd19fab67edca654618614beda32c14
Dylan Dunn <dhd5076@user.noreply.github.com>
2018-02-03 12:53:51 -0500

    Put MOTD in file

--- Smile/routes/index.js
+++ Smile/routes/index.js
@@ -1,9 +1,11 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var fs = require('fs')
 
 /* GET home page. */
 router.get('/', function (req, res) {
+    global.motd = fs.readFileSync('motd', 'utf8');
     res.render('index', { motd: global.motd });
 });
 
